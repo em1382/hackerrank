976d0f2d49cd9657060c74583554cd704b471d3f
Ellis Madagan <em1382@messiah.edu>
2017-07-19 11:20:46 -0400

    Create birthday-cake-candles.py

---
+++ algorithms/warmup/birthday-cake-candles.py
@@ -0,0 +1,7 @@
+def birthdayCakeCandles(n, ar):
+    return ar.count(max(ar))
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = birthdayCakeCandles(n, ar)
+print(result)
